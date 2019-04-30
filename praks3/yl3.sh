9e6483a574fb2c56420989ae5707767de3f52684
root <root@scriptnigol>
2019-04-30 13:28:26 +0300

    Lisatud paar kommentaari

--- praks3/yl3.sh
+++ praks3/yl3.sh
@@ -5,6 +5,10 @@
 #siit algab skript
 
 # väljutame, kui kaua arvuti tööl on olnud
+ # hostname on arvuti nimi
+ # date kuvab meile praeguse kellaja
+ # uptime annab meile minutites, tundides, päevades või kuudes uptime
 
 echo `hostname` "at" `date +"%H:%M:%S"` `uptime -p`
 
+# siit lõppeb skript
