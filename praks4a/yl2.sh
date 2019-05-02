b4e05f362ebd41e023493400ce5f57f8f6d7de4c
root <root@scriptnigol>
2019-05-02 11:38:43 +0300

    Ringi pindala skript

---
+++ praks4a/yl2.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# siit algab skript
+# see skript aitab arvutada ringi pindala ja ümbermõõtu.
+
+# küsime kasutajalt parameetrid
+
+echo -n "Palun sisestage ringi raadius(sentimeetrites): "
+read realraadius
+
+# hakkame arvutama pindala ja ümbermõõtu
+# arvutame raadiuse, pindala ja ümbermõõdu ja väljutame vastuse!
+raadius=`expr $realraadius \* $realraadius`
+pindala=$(echo "3.14 * $raadius" | bc)
+ymberm66t=$(echo "2 * 3.14 * $realraadius" | bc)
+
+
+echo "Ringi pindala on $pindala cm" 
+echo "Ringi ümbermõõt on $ymberm66t cm"
+
+#siit lõppeb skript
