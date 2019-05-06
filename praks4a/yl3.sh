2ddac91d6744cb26c06b78957acce62bb92f8080
root <root@scriptnigol>
2019-05-06 14:30:19 +0300

    Algne backup script

---
+++ praks4a/yl3.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# siit algab skript
+
+# see skript aitab teha backupe kaustadest ja failidest
+
+# installeerime kasutajale igaksjuhuks tar installeri, kui seda pole
+echo "Alustan tar-i installeerimisega. Kui sul on see juba olemas, on kõik korras."
+sleep 2s
+apt-get install tar >>/dev/null
+echo "Installeerimine lõpetatud."
+
+# küsime kasutajalt, mis kataloogi ta soovib backupida
+echo -n "Palun sisestage kataloogi, mida soovite backupida: "; read backup
+
+# küsime kasutajalt, kuhu soovitakse kataloog backupida 
+# echo -n "Palun sisestage kataloog, kuhu soovite backupi luua: "; read dest
+
+# loome failinime
+day=$(date +%A)
+host=$(hostname -s)
+failinimi="$host-$day.tgz"
+# anname kausta backup kinnituse
+dest=/home/backupid/
+echo
+sleep 1s
+echo "Hakkan kausta $backup backupima."
+sleep 1s
+echo "Uueks kausta asukohaks on $dest."
+sleep 1s
+echo "Failinimeks on $failinimi"
+sleep 1s
+
+# loome backupi kausta
+mkdir $dest 2>/dev/null
+
+# hakkame backupima kasutades tari
+tar -czf $failinimi $backup 
+mv $failinimi $dest/$failinimi
+
+# väljastame lõpu staatuse
+echo
+echo "Backup lõpetatud!"
+
+#skripti lõpp
