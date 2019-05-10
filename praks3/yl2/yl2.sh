ae43bbe32eb537579427188c1e1cda881e76ef40
root <root@scriptnigol>
2019-05-10 13:26:11 +0300

    .log failide sorteerimise skript ridade arvu alusel

--- praks3/yl2/yl2.sh
+++ praks3/yl2/yl2.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-# skript otsib var kataloogis kasutajale kättesaadavad .log lõpuga failid
-# kontrollib, mitu rida neis on ja väljastab failide nimekirja sorteeritud
-# kahanemisjärjekorras vastavalt ridade arvule.
-
-#siit hakkab skript
- 
-# otsime kõik .log lõpuga failid üles ja sorteerime need ära,
-# errorid saadame faili null, mis asub kaustas /dev
-find /var/ -name "*.log" -type f 2>> /dev/null -exec wc -l {} + | sort -rn
-
-#siit lõppeb skript 
+./logfiles.sh >> logifail.txt
